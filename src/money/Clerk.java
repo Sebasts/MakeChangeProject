c03fac1cb3586bb114367f516f67eb3e1cbde6a3
Sebastian Hellsing <sebastsing@gmail.com>
2017-04-07 15:19:03 -0600

    Created shel methods

--- src/money/Clerk.java
+++ src/money/Clerk.java
@@ -1,10 +1,45 @@
 package money;
 
+import java.util.Scanner;
+
 public class Clerk {
 
+	static Scanner in = new Scanner(System.in);
+	
+	static double cost;
+	static double cash;
+	
+	static String answer;
+	
+	static int pennies;
+	static int nickels;
+	static int dimes;
+	static int quarters;
+	static int dollars;
+	static int fives;
+	static int tens;
+	static int twenties;
+	static int fifties;
+		
+		
 	public static void main(String[] args) {
-		//I have made a change
+		
+		customerCheckOut();
 
 	}
+	
+	public static void customerCheckOut(){
+		System.out.println("Enter the price of your item.");
+		
+		cost += in.nextDouble();
+		
+	}
+	
+	public static void changeMaker(){
+		
+		
+	}
+	
+	public static void 
 
 }
