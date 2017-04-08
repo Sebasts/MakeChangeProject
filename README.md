a9553d17b33c7ffc4a6dbd8c87ff74f3bd82661a
Sebastian Hellsing <sebastsing@gmail.com>
2017-04-07 20:06:54 -0600

    Clean up of code and README update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Sebastian's Make Change Homework Project
+This project allows a customer to add items (by price) to a shopping cart,
+displays total, and handles the transaction including returning of change in the
+highest denominations possible.
--- src/money/Clerk.java
+++ src/money/Clerk.java
@@ -30,6 +30,7 @@ public class Clerk {
 
 	}
 
+	//Initial user interaction is handled here
 	public static void customerCheckOut() {
 		System.out.println("Do you want to ring up an item? y or n. n cancels this transaction\n");
 		answer = in.next();
@@ -46,58 +47,62 @@ public class Clerk {
 
 	}
 
+	//This method will decide how many of each unit of currency
+	//that the customer will be given as change
 	public static void changeMaker() {
 		
 		if (changeDue - 20 >= 0) {
 			twenties += (int) (changeDue / 20);
 			changeDue -= 20 * twenties;
-			System.out.println(changeDue+ "20");
+			
 			changeMaker();
 
 		} else if (changeDue - 10 >= 0) {
 			tens = (int) (changeDue / 10);
 			changeDue -= 10 * tens;
-			System.out.println(changeDue+"10");
+			
 			changeMaker();
 
 		} else if (changeDue - 5 >= 0) {
 			fives = (int) (changeDue / 5);
 			changeDue -= 5 * fives;
-			System.out.println(changeDue+"5");
+			
 			changeMaker();
 
 		} else if (changeDue - 1 >= 0) {
 			ones = (int) changeDue;
 			changeDue -= (int) changeDue;
-			System.out.println(changeDue+"1");
+			
 			changeMaker();
 
 		} else if (changeDue - .25 >= 0) {
 			quarters = (int) (changeDue / .25);
 			changeDue -= .25 * quarters;
-			System.out.println(changeDue+".25");
+			
 			changeMaker();
 
 		} else if (changeDue - .1 >= 0) {
 			dimes = (int) (changeDue /.1);
 			changeDue -= .1 * dimes;
-			System.out.println(changeDue+".1");
+			
 			changeMaker();
 
 		} else if (changeDue - .05 >= 0) {
 			nickels = (int) (changeDue / .05);
 			changeDue -= .05 * nickels;
-			System.out.println(changeDue+".05");
+			
 			changeMaker();
 
 		} else{ 
-			pennies = (int)(changeDue*101);
-			System.out.println(changeDue*101+".01");
+			pennies = (int)(changeDue*101); //*101 due to inaccuracy with math with doubles
+			
 		}
 		
 
 	}
 
+	//This method handles the addition of items to the user's cart
+	//and transaction cancellation.
 	public static void ringUp() {
 		System.out.println("Enter the price of your item.\n");
 
@@ -113,6 +118,8 @@ public class Clerk {
 
 		} else if (answer.equalsIgnoreCase("n")) {
 			payUp();
+			System.out.println("Your change is $" + change + " and is paid out in: \n");
+			printChange();
 
 		} else {
 			System.out.println("I did not reconize your choice, please try again.\n ");
@@ -120,6 +127,8 @@ public class Clerk {
 		}
 	}
 
+	//Handles the customer paying. Will handle transactions
+	//whether or not the customer has enough money
 	public static void payUp() {
 
 		System.out.println("Your total comes to $" + cost + " \n \n" + "Enter the amount that you are paying with.\n");
@@ -142,7 +151,7 @@ public class Clerk {
 		}
 
 	}
-
+	
 	public static void printChange() {
 
 		if (pennies > 0) {
@@ -169,8 +178,7 @@ public class Clerk {
 		if (twenties> 0){
 			System.out.print("Twenties: "+ twenties+" - ");			
 		}
-		System.out.println("Your change is $" + change + " and is paid out in: \n");
-		printChange();
+		
 	}
 
 }
